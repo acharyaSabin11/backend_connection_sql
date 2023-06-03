54954d9ef5095801bfea68e23101ec9f77c46464
acharyaSabin11 <sabin772419@thc.tu.edu.np>
2023-06-03 12:09:25 +0545

    Initial Setup

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const submitButton = document.querySelector('#submitButton');
+const textField = document.querySelector('#textField');
+
+submitButton.addEventListener('click' , ()=> console.log('Button Check'));
